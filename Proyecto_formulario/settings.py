680f66babd456a2a2cdad86fefdffb76e2946cfc
fgma-aplicacionesweb <aplicacioneswebfundayacucho@gmail.com>
2025-08-08 03:12:56 -0400

    funciona con postgres

--- Proyecto_formulario/settings.py
+++ Proyecto_formulario/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Proyecto_formulario.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fgma_egresados',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
